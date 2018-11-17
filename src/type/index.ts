ef52b6c66899825a1c972e2796504a74dc903bd2
IInfinity <I.INF@Outlook.com>
2018-11-17 22:57:51 +0800

    Fix: Fixed no such prop bug.

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,8 +1,22 @@
 import { IMiddleware } from 'koa-router';
+import { Middleware } from 'koa-compose';
+import { Files } from 'formidable';
+import { Context, Request } from 'koa';
+
+export type AMiddleware = Middleware<AContext>;
+
+interface AContext extends Context {
+  request: ARequest;
+}
+
+interface ARequest extends Request {
+  body: any;
+  files: Files;
+}
 
 /** 路径名: 路径处理方式. */
 export interface RouterPaths {
-  [index: string]: IMiddleware;
+  [index: string]: IMiddleware | AMiddleware | any;
 }
 
 /** 所有的路由路径. */
