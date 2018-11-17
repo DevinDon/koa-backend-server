0ff2cf4b58c7a112e9aea1fd406ecec03c484b8a
DevinDon <DevinDon@Foxmail.com>
2018-11-17 22:39:54 +0800

    Addition: Add default config, optimized.

--- src/koa/Server.ts
+++ src/koa/Server.ts
@@ -3,12 +3,14 @@ import HTTP2 from 'http2';
 import HTTPS from 'https';
 import Koa, { Middleware } from 'koa';
 import { now } from '../util';
+import { Router, Session } from '../middleware';
+import { AllPaths } from '../type';
 
 /** Koa Server. */
 export class Server {
 
   /** Koa. */
-  private app: Koa;
+  private application: Koa;
   /** Server. */
   private server: HTTP.Server | HTTP2.Http2SecureServer | HTTPS.Server;
 
@@ -17,22 +19,37 @@ export class Server {
    * @param type 服务器类型, HTTP | HTTPS | HTTP2.
    */
   constructor(type: 'HTTP' | 'HTTP2' | 'HTTPS' = 'HTTP', options?: HTTP2.ServerOptions | HTTPS.ServerOptions) {
-    this.app = new Koa();
+    this.application = new Koa();
     switch (type) {
       case 'HTTP':
-        this.server = HTTP.createServer(this.app.callback());
+        this.server = HTTP.createServer(this.application.callback());
         break;
       case 'HTTP2':
-        this.server = HTTP2.createSecureServer(<HTTP2.ServerOptions>options, this.app.callback());
+        this.server = HTTP2.createSecureServer(<HTTP2.ServerOptions>options, this.application.callback());
         break;
       case 'HTTPS':
-        this.server = HTTPS.createServer(<HTTPS.ServerOptions>options, this.app.callback());
+        this.server = HTTPS.createServer(<HTTPS.ServerOptions>options, this.application.callback());
         break;
       default:
-        this.server = HTTP.createServer(this.app.callback());
+        this.server = HTTP.createServer(this.application.callback());
         console.error(`No such server type: ${type}, use default HTTP server.`);
         break;
     }
+  }
+
+  /**
+   * Default setting.
+   * @param paths Router paths.
+   * @param keys Session Key.
+   * @param port HTTP(2/S) port.
+   */
+  public default(paths: AllPaths = {}, keys: string[] = ['default'], port: number = 80): void {
+    const session = new Session(this.application, keys);
+    const router = new Router(paths);
+
+    this.use(session.ware, router.ware);
+
+    this.listen(port);
   }
 
   /**
@@ -41,7 +58,7 @@ export class Server {
    */
   public use(...middlewares: Middleware[]): void {
     for (const middleware of middlewares) {
-      this.app.use(middleware);
+      this.application.use(middleware);
       console.log(`${now()}: Use middleware: ${middleware.name || middleware.toString()}.`);
     }
   }
@@ -55,6 +72,12 @@ export class Server {
   public listen(port: number, host: string = 'localhost'): HTTP.Server | HTTP2.Http2SecureServer | HTTPS.Server {
     return this.server.listen(port, host, () => console.log(`${now()}: Server online, address is ${host}:${port}.`));
   }
+
+
+  public get app(): Koa {
+    return this.application;
+  }
+
 
 }
 
