5a6c6ef2f3dc6db9fc36cd5a43828af8376e158f
IInfinity <I.INF@Outlook.com>
2018-11-20 20:57:05 +0800

    Addition: KBS config type.

--- src/type/index.ts
+++ src/type/index.ts
@@ -2,7 +2,10 @@ import { IMiddleware } from 'koa-router';
 import { Middleware } from 'koa-compose';
 import { Files } from 'formidable';
 import { Context, Request } from 'koa';
+import { ServerOptions } from 'https';
+import { SecureServerOptions } from 'http2';
 
+/** Router path 中间件, 包含 Session 和 Post data. */
 export type AMiddleware = Middleware<AContext>;
 
 interface AContext extends Context {
@@ -12,6 +15,22 @@ interface AContext extends Context {
 interface ARequest extends Request {
   body: any;
   files: Files;
+}
+
+/** KBS config. */
+export interface KBSConfig {
+  /** Type of KBS, default to 'HTTP'. */
+  type?: 'HTTP' | 'HTTPS' | 'HTTP2';
+  /** Cookie & Session secret keys. */
+  keys?: string[];
+  /** HTTPS / HTTP2 options. */
+  options?: ServerOptions | SecureServerOptions;
+  /** Router paths. */
+  paths?: AllPaths;
+  /** Port. */
+  port?: number;
+  /** Host. */
+  host?: string;
 }
 
 /** 路径名: 路径处理方式. */
