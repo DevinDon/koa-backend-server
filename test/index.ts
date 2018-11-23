52bdf1e819806f451afbe6f4da891f1f2dcfa89d
IInfinity <I.INF@Outlook.com>
2018-11-23 22:56:01 +0800

    Update: Use constructor to connect database.

--- test/index.ts
+++ test/index.ts
@@ -1,24 +1,45 @@
-import { Database, Server } from '../dist';
+import { ConnectionOptions } from 'typeorm';
+import { AllPaths, Server } from '../dist';
 import { User } from './entity';
-import { postPaths } from './post';
 
-const database = new Database({
-  name: 'default',
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'docker',
-  password: 'docker',
-  database: 'docker',
-  entities: [
+const database: ConnectionOptions = {
+  name: 'default', // use 'default'
+  type: 'mysql', // database type
+  host: 'localhost', // database host
+  port: 3306, // database port
+  username: 'user', // database user
+  password: 'password', // database password
+  database: 'database', // database name
+  entities: [ // entites, means table(SQL) or document(NOSQL)
     User
   ],
-  synchronize: true,
-  logging: true
-});
+  synchronize: false, // only turn on when you're in dev
+  logging: false // show query logs
+};
 
-const server = new Server({
-  type: 'HTTP',
-  keys: ['test'],
-  paths: { POST: postPaths }
+const keys: string[] = ['your', 'secret', 'keys'];
+
+const paths: AllPaths = {
+  GET: {
+    '/path/you/wanna/get': (ctx, next) => { /* ... */ },
+    '/path/other': (ctx, next) => { /* ... */ }
+  }, POST: {
+    '/path/you/wanna/post': (ctx, next) => { /* ... */ }
+  }, PUT: {
+    /* ... */
+  }
+};
+
+const type: 'HTTP' | 'HTTPS' | 'HTTP2' = 'HTTP';
+
+const host: string = 'localhost';
+const port: number = 8080;
+
+const server: Server = new Server({
+  database,
+  host,
+  keys,
+  paths,
+  port,
+  type
 });
