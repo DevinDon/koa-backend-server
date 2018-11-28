4be6763ff28465c8ba38ebd5a8db4083e401c0ae
DevinDon <DevinDon@Foxmail.com>
2018-11-28 09:11:25 +0800

    Update: Update test sample.

--- test/index.ts
+++ test/index.ts
@@ -1,39 +1,25 @@
-import { ConnectionOptions } from 'typeorm';
 import { AllPaths, Server } from '../dist';
 import { User } from './entity';
+import postPaths from './post';
 
-const database: ConnectionOptions = {
-  name: 'default', // use 'default'
-  type: 'mysql', // database type
-  host: 'localhost', // database host
-  port: 3306, // database port
-  username: 'user', // database user
-  password: 'password', // database password
-  database: 'database', // database name
-  entities: [ // entites, means table(SQL) or document(NOSQL)
-    User
-  ],
-  synchronize: false, // only turn on when you're in dev
-  logging: false // show query logs
-};
-
+const database: boolean = true;
+const host: string = 'localhost';
 const keys: string[] = ['your', 'secret', 'keys'];
-
 const paths: AllPaths = {
   GET: {
-    '/path/you/wanna/get': (ctx, next) => { /* ... */ },
-    '/path/other': (ctx, next) => { /* ... */ }
-  }, POST: {
-    '/path/you/wanna/post': (ctx, next) => { /* ... */ }
-  }, PUT: {
-    /* ... */
-  }
+    '/': async (c, next) => {
+      const result = User.find();
+      c.body = {
+        status: result ? true : false,
+        data: result
+      };
+      next();
+    }
+  },
+  POST: postPaths
 };
-
-const type: 'HTTP' | 'HTTPS' | 'HTTP2' = 'HTTP';
-
-const host: string = 'localhost';
 const port: number = 8080;
+const type: 'HTTP' | 'HTTPS' | 'HTTP2' = 'HTTP';
 
 const server: Server = new Server({
   database,
