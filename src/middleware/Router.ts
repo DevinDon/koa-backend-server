44813d5aaa9ce2e797b5449489791fc12aadf817
DevinDon <DevinDon@Foxmail.com>
2018-11-29 23:48:26 +0800

    Update: Source code update. # 0.2.3 => 0.2.4 - Now you can set your API version as the perfix. - RESTful methods supported.

--- src/middleware/Router.ts
+++ src/middleware/Router.ts
@@ -14,9 +14,16 @@ export class Router extends KoaRouter {
    * Generate router.
    * @param {AllPaths} allPaths All router paths.
    */
-  constructor(allPaths: AllPaths = {}) {
+  constructor(allPaths?: AllPaths, version?: string) {
     super();
-    this.loadAllPaths(allPaths);
+    if (allPaths) {
+      this.loadAllPaths(allPaths);
+      console.log(`${now()}\tLoaded router paths`);
+    }
+    if (version) {
+      this.prefix(`/${version}`);
+      console.log(`${now()}\tAPI version: ${version}, now you can access your router paths with prefix /${version}`);
+    }
   }
 
   /**
@@ -25,9 +32,27 @@ export class Router extends KoaRouter {
    * @returns {void} void.
    */
   public loadAllPaths(paths: AllPaths): void {
+    this.loadDeletePaths(paths.DELETE || {});
     this.loadGetPaths(paths.GET || {});
+    this.loadHeadPaths(paths.HEAD || {});
+    this.loadOptionsPaths(paths.OPTIONS || {});
+    this.loadPatchPaths(paths.PATCH || {});
     this.loadPostPaths(paths.POST || {});
     this.loadPutPaths(paths.PUT || {});
+  }
+
+  /**
+   * Load Delete Method Paths.
+   * @param {RouterPaths} paths Delete Method Paths.
+   * @returns {void} void.
+   */
+  public loadDeletePaths(paths: RouterPaths): void {
+    for (const key in paths) {
+      if (paths.hasOwnProperty(key)) {
+        this.delete(key, paths[key]);
+        console.log(`${now()}\tRegister Router Delete: ${key}`);
+      }
+    }
   }
 
   /**
@@ -39,7 +64,49 @@ export class Router extends KoaRouter {
     for (const key in paths) {
       if (paths.hasOwnProperty(key)) {
         this.get(key, paths[key]);
-        console.log(`${now()}: Register Router GET: ${key}`);
+        console.log(`${now()}\tRegister Router GET: ${key}`);
+      }
+    }
+  }
+
+  /**
+   * Load Head Method Paths.
+   * @param {RouterPaths} paths Head Mothod Paths.
+   * @returns {void} void.
+   */
+  public loadHeadPaths(paths: RouterPaths): void {
+    for (const key in paths) {
+      if (paths.hasOwnProperty(key)) {
+        this.head(key, paths[key]);
+        console.log(`${now()}\tRegister Router Head: ${key}`);
+      }
+    }
+  }
+
+  /**
+   * Load Options Method Paths.
+   * @param {RouterPaths} paths Options Mothod Paths.
+   * @returns {void} void.
+   */
+  public loadOptionsPaths(paths: RouterPaths): void {
+    for (const key in paths) {
+      if (paths.hasOwnProperty(key)) {
+        this.options(key, paths[key]);
+        console.log(`${now()}\tRegister Router Options: ${key}`);
+      }
+    }
+  }
+
+  /**
+   * Load Patch Method Paths.
+   * @param {RouterPaths} paths Patch Mothod Paths.
+   * @returns {void} void.
+   */
+  public loadPatchPaths(paths: RouterPaths): void {
+    for (const key in paths) {
+      if (paths.hasOwnProperty(key)) {
+        this.patch(key, paths[key]);
+        console.log(`${now()}\tRegister Router Patch: ${key}`);
       }
     }
   }
@@ -53,7 +120,7 @@ export class Router extends KoaRouter {
     for (const key in paths) {
       if (paths.hasOwnProperty(key)) {
         this.post(key, KoaBody(), paths[key]);
-        console.log(`${now()}: Register Router POST: ${key}`);
+        console.log(`${now()}\tRegister Router POST: ${key}`);
       }
     }
   }
@@ -67,7 +134,7 @@ export class Router extends KoaRouter {
     for (const key in paths) {
       if (paths.hasOwnProperty(key)) {
         this.post(key, paths[key]);
-        console.log(`${now()}: Register Router PUT: ${key}`);
+        console.log(`${now()}\tRegister Router PUT: ${key}`);
       }
     }
   }
