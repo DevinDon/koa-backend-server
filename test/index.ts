0f0f3dd33c4e5c992710bdb51e05c08344c6e49b
DevinDon <DevinDon@Foxmail.com>
2018-12-02 14:27:51 +0800

    Update: Update test unit.

--- test/index.ts
+++ test/index.ts
@@ -1,24 +1,20 @@
 import { AllPaths, Server } from '../src';
 import postPaths from './post';
 
-const database: boolean = false;
-
-const keys: string[] = ['your', 'secret', 'keys'];
-
-const paths: AllPaths = {
-  POST: postPaths
-};
-
-const host: string = 'localhost';
-const port: number = 80;
-const type: 'HTTP' | 'HTTPS' | 'HTTP2' = 'HTTP';
-
-const server: Server = new Server({
-  database,
-  host,
-  keys,
-  paths,
-  port,
-  type,
-  version: 'v1'
+const server = new Server({
+  address: {
+    portocol: 'HTTP'
+  },
+  // database: {
+  //   ormconfig: true
+  // },
+  router: {
+    paths: {
+      POST: postPaths
+    },
+    version: 'v1'
+  },
+  session: {
+    keys: ['test', 'keys']
+  }
 });
