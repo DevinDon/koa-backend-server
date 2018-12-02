6c3465762900a327697a3927ddbde47270fd1f27
DevinDon <DevinDon@Foxmail.com>
2018-12-02 15:26:42 +0800

    Optimization: Separation definition of KBSConfig.

--- src/type/server.ts
+++ src/type/server.ts
@@ -3,36 +3,48 @@ import { ServerOptions } from 'https';
 import { ConnectionOptions } from 'typeorm';
 import { AllPaths } from './router';
 
+/** KBS address. */
+export interface KBSAddress {
+  /** KBS portocol. */
+  portocol: 'HTTP' | 'HTTPS' | 'HTTP2';
+  /** KBS host. */
+  host?: string;
+  /** KBS port. */
+  port?: number;
+  /** KBS ssl options, only need in secure server (HTTPS and HTTP2). */
+  ssl?: ServerOptions | SecureServerOptions;
+}
+
+/** KBS database connection, if undefined it will disable the typeorm connection. */
+export interface KBSDatabase {
+  /** Use the ormconfig.json file to connect database. */
+  ormconfig?: boolean;
+  /** Use your own options to connect database. */
+  options?: ConnectionOptions;
+}
+
+/** KBS router, if undefined it will disable the koa router. */
+export interface KBSRouter {
+  /** All paths of KBS router. */
+  paths: AllPaths;
+  /** API version, prefix of all paths. */
+  version?: string;
+}
+
+/** KBS session, if undefined it will disable the koa session. */
+export interface KBSSession {
+  /** KBS session keys, encrypted cookies. */
+  keys: string[];
+}
+
 /** KBS config. */
 export interface KBSConfig {
   /** KBS address. */
-  address: {
-    /** KBS portocol. */
-    portocol: 'HTTP' | 'HTTPS' | 'HTTP2';
-    /** KBS host. */
-    host?: string;
-    /** KBS port. */
-    port?: number;
-    /** KBS ssl options, only need in secure server (HTTPS and HTTP2). */
-    ssl?: ServerOptions | SecureServerOptions;
-  };
+  address: KBSAddress;
   /** KBS database connection, if undefined it will disable the typeorm connection. */
-  database?: {
-    /** Use the ormconfig.json file to connect database. */
-    ormconfig?: boolean;
-    /** Use your own options to connect database. */
-    options?: ConnectionOptions;
-  };
+  database?: KBSDatabase;
   /** KBS router, if undefined it will disable the koa router. */
-  router?: {
-    /** All paths of KBS router. */
-    paths: AllPaths;
-    /** API version, prefix of all paths. */
-    version?: string;
-  };
+  router?: KBSRouter;
   /** KBS session, if undefined it will disable the koa session. */
-  session?: {
-    /** KBS session keys, encrypted cookies. */
-    keys: string[];
-  };
+  session?: KBSSession;
 }
