017530aab59b61d117be9567e229adb3f014e0a5
DevinDon <DevinDon@Foxmail.com>
2018-12-22 23:50:17 +0800

    Update: Update sample code.

--- test/index.ts
+++ test/index.ts
@@ -3,36 +3,46 @@ import postPaths from './post';
 
 const server = new Server({
   address: {
-    portocol: 'HTTP', // required, HTTP, HTTPS or HTTP2
-    host: '0.0.0.0', // optional, default to 0.0.0.0
-    port: 8080, // optional, default to 80
-    // ssl: {cert: 'CERT', key: 'KEY'} // required if portocol is HTTPS or HTTP2
+    portocol: 'HTTP', // Required, HTTP, HTTPS or HTTP2.
+    host: '0.0.0.0', // Optional, default to 0.0.0.0.
+    port: 80, // Optional, default to 80.
+    // ssl: {cert: 'CERT', key: 'KEY'} // Required if portocol is HTTPS or HTTP2.
   },
-  // database: { // if undefined, it will disable database connection
-  //   ormconfig: true, // if true, it will use ormconfig.json to connect database, and the connection options will be ignore
-  //   options: {
-  //     name: 'default',
-  //     type: 'mysql',
-  //     host: 'localhost',
-  //     port: 3306,
-  //     database: 'database',
-  //     username: 'username',
-  //     password: 'password',
-  //     synchronize: true, // auto generate database table (or document), but you may lost all data of this database
-  //     logging: true, // log all query statements
-  //     entities: [/** your own entities */]
-  //   }
-  // },
-  router: { // if undefined, it will disable koa router
+  database: { // If undefined, it will disable database connection
+    ormconfig: true, // If true, it will use ormconfig.json to connect database, and the connection options will be ignore
+    // options: { // Your own database options.
+    //   name: 'default',
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   database: 'database',
+    //   username: 'username',
+    //   password: 'password',
+    //   synchronize: true, // auto generate database table (or document), but you may lost all data of this database
+    //   logging: true, // log all query statements
+    //   entities: [/** your own entities */]
+    // }
+  },
+  router: { // If undefined, it will disable koa router.
     paths: { // router paths
       POST: postPaths
     },
-    static: {
-      path: 'test/'
+    static: { // Static files path.
+      path: 'test/html/'
     },
-    version: 'v1' // API version, the prefix of all paths
+    version: 'v1' // API version, the prefix of all paths.
   },
-  session: { // if undefined, it will disable koa session
-    keys: ['your', 'secret', 'keys'] // session keys to encrypt the cookies
+  session: { // If undefined, it will disable redisession.
+    name: 'session.id',
+    domain: 'your.domain',
+    httpOnly: true,
+    secert: ['keys'],
+    redis: {
+      host: 'your.redis.address',
+      port: 6379
+    }
   }
 });
+
+// Start listening.
+server.listen();
