a461d7314fbe0dc0d48d4bcdf2b7bc480521266e
IInfinity <DevinDon@Foxmail.com>
2019-03-26 01:02:54 +0800

    perf(server): use logger as default console

--- src/koa/Server.ts
+++ src/koa/Server.ts
@@ -1,3 +1,4 @@
+import { logger } from '@iinfinity/logger';
 import { readFileSync } from 'fs';
 import HTTP from 'http';
 import HTTP2 from 'http2';
@@ -8,7 +9,6 @@ import { RediSession } from 'redisession';
 import { KBSConfig } from '../@types';
 import { Database } from '../database';
 import { Router } from '../middleware';
-import { now } from '../util';
 
 /**
  * KBS, Koa Backend Server.
@@ -30,6 +30,7 @@ export class Server {
 
   /**
    * Create a KBS, Koa Backend Server.
+   *
    * @param {KBSConfig} config KBS Server options.
    */
   constructor(config?: KBSConfig) {
@@ -37,7 +38,7 @@ export class Server {
     try {
       // Default in development mode, use `npm start prod` to enable production mode.
       const prod = Boolean(process.argv.find(v => v === 'prod'));
-      console.log(`${now()}\tKoa Backend Server is on ${prod ? 'production' : 'development'} mode.`);
+      logger.info(`Koa Backend Server is on ${prod ? 'production' : 'development'} mode.`);
       const json: KBSConfig = JSON.parse(readFileSync(prod ? 'server.config.json' : 'server.config.dev.json').toString());
       // The profile will cover config.
       this.config = Object.assign({}, json, config);
@@ -47,7 +48,7 @@ export class Server {
         }
       }
     } catch (err) {
-      console.log(`${now()}\tProfile server.config.json not found or cannot be parse, disable it. Detail: ${err}`);
+      logger.info(`Profile server.config.json not found or cannot be parse, disable it. Detail: ${err}`);
       this.config = config || {};
     }
     // Init KBS.
@@ -56,6 +57,7 @@ export class Server {
 
   /**
    * Init KBS.
+   *
    * @returns {Promise<void>} Void.
    */
   private async init(): Promise<void> {
@@ -75,20 +77,20 @@ export class Server {
           break;
         default:
           this.server = HTTP.createServer(this.application.callback());
-          console.log(`${now()}\tUnkoown portocol or unset portocol: ${this.config.address.portocol}, use default portocol HTTP`);
+          logger.warn(`Unkoown portocol or unset portocol: ${this.config.address.portocol}, use default portocol HTTP`);
           break;
       }
       this.app.proxy = Boolean(this.config.address.proxy);
     } else { // Default to HTTP.
       this.server = HTTP.createServer(this.application.callback());
-      console.log(`${now()}\tUse default portocol HTTP`);
+      logger.info(`Use default portocol HTTP`);
     }
 
     // Create database connection or not.
     if (this.config.database) {
       this.database = new Database(this.config.database);
     } else {
-      console.log(`${now()}\tDatabase not connected.`);
+      logger.warn(`Database not connected.`);
     }
 
     // Use session middleware or not.
@@ -96,7 +98,7 @@ export class Server {
       this.session = new RediSession(this.application, this.config.session);
       this.use(this.session.ware);
     } else {
-      console.log(`${now()}\tSession service not provided.`);
+      logger.warn(`Session service not provided.`);
     }
 
     // Config router or not.
@@ -105,45 +107,45 @@ export class Server {
       this.use(this.router.ware);
       if (this.config.router.static && this.config.router.static.path) {
         this.use(KoaStatic(this.config.router.static.path, this.config.router.static.options));
-        console.log(`${now()}\tStatic resource path: ${this.config.router.static.path}`);
+        logger.info(`Static resource path: ${this.config.router.static.path}`);
       } else {
-        console.log(`${now()}\tStatic server service not provided.`);
+        logger.info(`Static server service not provided.`);
       }
     } else {
-      console.warn(`${now()}\tRouting service not provided.`);
+      logger.warn(`Routing service not provided.`);
     }
 
   }
 
   /**
    * Use middlewares.
+   *
    * @param {Middleware[]} middlewares Middlewares.
    * @returns {Server} This server.
    */
   public use(...middlewares: Middleware[]): Server {
     for (const middleware of middlewares) {
       this.application.use(middleware);
-      console.log(`${now()}\tUse middleware: ${middleware.name || middleware.toString()}`);
+      logger.info(`Use middleware: ${middleware.name || middleware.toString()}`);
     }
     return this;
   }
 
   /**
    * Listening on some where.
+   *
    * @param {number} port Listening port, default to 8080.
    * @param {string} host The listening host, default to 0.0.0.0.
    * @returns {Promise<Server>} This server.
    */
   public async listen(host?: string, port?: number): Promise<Server> {
     if (this.database) {
-      console.log(`${now()}\tConnecting to database, please wait...`);
       await this.database.connect();
-      console.log(`${now()}\tDatabase connected.`);
     }
     this.server.listen(
       port = port || (this.config.address && this.config.address.port) || 8080,
       host = host || (this.config.address && this.config.address.host) || '0.0.0.0',
-      () => console.log(`${now()}\tServer online, address is ${host}:${port}`)
+      () => logger.info(`Server online, address is ${host}:${port}`)
     );
     return this;
   }
