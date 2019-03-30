22d4564a98aa915371557489a18bf67474529ec6
IInfinity <I.INF@Outlook.com>
2019-03-30 22:27:38 +0800

    perf(demo): use src/main

--- src/demo/simple/index.ts
+++ src/demo/simple/index.ts
@@ -1,4 +1,4 @@
-import { RouterPaths, Server, ServerConfig } from '../../../dist'; // Replace it with '@iinfinity/rester' on your workspace.
+import { RouterPaths, Server, ServerConfig } from '../../main'; // Replace it with '@iinfinity/rester' on your workspace.
 
 const GET: RouterPaths = {
   '/ get index': {
