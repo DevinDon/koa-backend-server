90d7efc435849b676bd597e764f1210c66705780
IInfinity <DevinDon@Foxmail.com>
2019-06-01 00:44:04 +0800

    perf(draft): injection draft

--- src/main/service/Service.ts
+++ src/main/service/Service.ts
@@ -1,37 +1,23 @@
 import 'reflect-metadata';
-import { CORSOption, Method, Path } from '../@types';
-import { Router } from './router';
+import { Method, Path } from '../@types';
 import { DI } from '../decorator/DI';
 
 const ROUTER$SERVICE = 'ROUTER:SERVICE';
 const ROUTER$CORS = 'ROUTER:CORS';
 const ROUTER$METHOD = 'ROUTER:METHOD';
 const ROUTER$PATH = 'ROUTER:PATH';
+const ROUTER$SESSION = 'ROUTER:SESSION';
 
-function baseMethod(method: Method) {
-  return (path: string = ''): MethodDecorator => {
-    return (target, name, descriptor) => {
-      Reflect.defineMetadata(ROUTER$PATH, path, descriptor.value!);
-      Reflect.defineMetadata(ROUTER$METHOD, method, descriptor.value!);
-    };
-  };
+function baseParam(metadataKey: any) {
+  return (): ParameterDecorator => (target, key, index) => Reflect.defineMetadata(metadataKey, index, target, key);
 }
 
-export function Service(path: string = ''): ClassDecorator {
-  return target => {
-    Reflect.defineMetadata(ROUTER$SERVICE, path, target);
-    const service = DI.generate(target);
-    const paths: Path[] = Object.getOwnPropertyNames(target.prototype)
-      .filter(v => v !== 'constructor')
-      .map<Path>(name => ({
-        method: Reflect.getMetadata(ROUTER$METHOD, target.prototype[name]),
-        value: Reflect.getMetadata(ROUTER$PATH, target.prototype[name]),
-        ware: async (context, next) => {
-          await next();
-          context.body = service[name].bind(service);
-        }
-      }));
-    Router.load(...paths);
+export const Session = baseParam(ROUTER$SESSION);
+
+function baseMethod(method: Method) {
+  return (path: string = ''): MethodDecorator => (target, name, descriptor) => {
+    Reflect.defineMetadata(ROUTER$PATH, path, descriptor.value!);
+    Reflect.defineMetadata(ROUTER$METHOD, method, descriptor.value!);
   };
 }
 
@@ -42,6 +28,26 @@ export const Options = baseMethod('OPTIONS');
 export const Patch = baseMethod('PATCH');
 export const Post = baseMethod('POST');
 export const Put = baseMethod('PUT');
+
+export function Service(path: string = ''): ClassDecorator {
+  return target => {
+    Reflect.defineMetadata(ROUTER$SERVICE, path, target);
+    const service = DI.generate(target);
+    const paths: Path[] = Object.getOwnPropertyNames(target.prototype)
+      .filter(v => v !== 'constructor')
+      .map<Path>(name => ({
+        method: Reflect.getMetadata(ROUTER$METHOD, target.prototype[name]),
+        path: Reflect.getMetadata(ROUTER$PATH, target.prototype[name]),
+        // ware: async (context, next) => {
+        //   await next();
+        //   context.body = service[name].bind(service);
+        // }
+        name: ''
+      }));
+    console.log('');
+    // Router.load(...paths);
+  };
+}
 
 // export function CORS(option: CORSOption): MethodDecorator {
 //   return (target, name, descriptor) => Reflect.defineMetadata(ROUTER$CORS, option, descriptor.value!);
@@ -56,7 +62,9 @@ class ABC {
   //   'Access-Control-Allow-Methods': [],
   //   'Access-Control-Allow-Origin': ''
   // })
-  def() { }
+  def(@Session() session: any) {
+    session.hello();
+  }
 
   @Post('/ghi')
   ghi() { }
