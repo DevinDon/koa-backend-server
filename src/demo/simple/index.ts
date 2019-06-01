cd622b191b0bf0a53f533ae3d58130eeec617841
IInfinity <DevinDon@Foxmail.com>
2019-06-02 03:49:27 +0800

    feat(demo): add a new simple demo

--- src/demo/simple/index.ts
+++ src/demo/simple/index.ts
@@ -1,24 +1,56 @@
-// import { RouterPaths, Rester, Option } from '@iinfinity/rester';
-import { Option, Rester, RouterPaths } from '../../main'; // use above import statement in your workspace
+import { IncomingMessage, Server, ServerResponse } from 'http';
+import 'reflect-metadata';
+import { Controller, GET, CoreHandler, CoreHandlerPool, HTTPRequest, HTTPResponse, Injectable, Injector, POST, RequestHeader } from '../../main';
 
-/** Router path. */
-const GET: RouterPaths = {
-  '/ get index': {
-    path: '/',
-    ware: async (c, next) => {
-      next();
-      c.body = 'Hello, world!';
-    }
+const pool: CoreHandlerPool = Injector.generate(CoreHandlerPool);
+
+@Controller()
+class DemoController {
+
+  private count = 0;
+
+  @GET('/')
+  index(): string {
+    return new Date().toLocaleString();
   }
-};
 
-/** Simple option. */
-const option: Option = {
-  router: {
-    paths: { GET }
+  @POST('/add')
+  add(): number {
+    return ++this.count;
   }
-};
 
-const server = new Rester(option);
+  @GET('/host')
+  host(@RequestHeader('host') host: string): string {
+    return host;
+  }
 
-server.listen();
+  @GET('/request')
+  request(@HTTPRequest() request: IncomingMessage): string {
+    return request.headers as any;
+  }
+
+  @GET('/response')
+  response(@HTTPResponse() response: ServerResponse): number {
+    response.writeHead(401, 'This is a long test reason, /adwdad/awd/wada/da/d/w/da//a/abc not found.');
+    return 401;
+  }
+
+}
+
+interface Plugin {
+  core: (handler: CoreHandler) => any;
+}
+
+@Injectable()
+class ExceptionPlugin implements Plugin {
+  core(handler: CoreHandler) {
+    // handler.handle;
+  }
+}
+
+const server = new Server((request, response) => {
+  const handler = pool.take(request, response);
+  response.end(handler.handle());
+  // don't need init
+  pool.give(handler);
+}).listen(8080);
