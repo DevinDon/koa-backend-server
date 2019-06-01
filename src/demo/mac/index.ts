8f7d4c8d83bcf9738ba595c60e24ed8649c8e1be
IInfinity <DevinDon@Foxmail.com>
2019-06-01 08:41:38 +0800

    perf(demo): new project structure, MAC
    MAC: Model-Aspect-Controller
    Model: Entity & Model
    Aspect: Guard & Plugin
    Controller: Service & Controller

---
+++ src/demo/mac/index.ts
@@ -0,0 +1,7 @@
+export * from './@types';
+export * from './controller';
+export * from './entity';
+export * from './guard';
+export * from './model';
+export * from './plugin';
+export * from './service';
