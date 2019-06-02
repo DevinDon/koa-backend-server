3556f3a4e41bb30cecaa45044a3087b391d12b2a
IInfinity <DevinDon@Foxmail.com>
2019-06-03 02:57:51 +0800

    fix(decorator): remove decorator Handler

--- src/main/decorator/Handler.ts
+++ src/main/decorator/Handler.ts
@@ -1,12 +1,7 @@
-import { HandlerPool } from '../handler/HandlerPool';
-import { Injector } from './Injector';
-import { MetadataKey } from '../@types';
-import { BaseHandler } from '../handler';
-
 export function Handler(): ClassDecorator {
   return target => {
-    const pool: HandlerPool = Injector.generate(HandlerPool);
-    const handlers: BaseHandler[] = Reflect.getMetadata(MetadataKey.Handler, HandlerPool) || [];
-    Reflect.defineMetadata(MetadataKey.Handler, handlers, HandlerPool);
+    // const pool: HandlerPool = Injector.generate(HandlerPool);
+    // const handlers: BaseHandler[] = Reflect.getMetadata(MetadataKey.Handler, HandlerPool) || [];
+    // Reflect.defineMetadata(MetadataKey.Handler, handlers, HandlerPool);
   };
 }
--- src/main/handler/index.ts
+++ src/main/handler/index.ts
@@ -1,3 +1,4 @@
 export * from './BaseHandler';
 export * from './CoreHandler';
 export * from './CoreHandlerPool';
+export * from './HandlerPool';
