22a76ca6201b8fadf7c2ea1954245227edf3a36c
IInfinity <I.INF@Outlook.com>
2019-06-08 06:19:11 +0800

    refactor(handler): refactor static method & properties

--- src/main/handler/HandlerPool.ts
+++ src/main/handler/HandlerPool.ts
@@ -5,24 +5,24 @@ import { CoreHandler } from './CoreHandler';
 
 export class HandlerPool {
 
-  static max = 100 * 100;
-  static order: HandlerType[] = [CoreHandler];
-  static pools: Map<string, BaseHandler[]> = new Map();
+  private max = 100 * 100;
+  private order: HandlerType[] = [CoreHandler, CoreHandler];
+  private pools: Map<string, BaseHandler[]> = new Map();
 
-  static take<T = BaseHandler>(type: HandlerType): T {
-    const pool = HandlerPool.pools.get(type.name)! || HandlerPool.pools.set(type.name, []).get(type.name)!;
+  take<T = BaseHandler>(type: HandlerType): T {
+    const pool = this.pools.get(type.name)! || this.pools.set(type.name, []).get(type.name)!;
     return pool.pop() || new type() as any;
   }
 
-  static give(handler: BaseHandler): number {
-    const pool = HandlerPool.pools.get(handler.constructor.name)! || HandlerPool.pools.set(handler.constructor.name, []).get(handler.constructor.name)!;
-    return pool.length < HandlerPool.max ? pool.push(handler.init()) : pool.length;
+  give(handler: BaseHandler): number {
+    const pool = this.pools.get(handler.constructor.name)! || this.pools.set(handler.constructor.name, []).get(handler.constructor.name)!;
+    return pool.length < this.max ? pool.push(handler.init()) : pool.length;
   }
 
-  static async process(option: HandlerOption): Promise<void> {
+  async process(option: HandlerOption): Promise<void> {
     // TODO: refactor
     try {
-      const result = await HandlerPool.compose(HandlerPool.take(HandlerPool.order[0]).init(option), 0)();
+      const result = await this.compose(this.take(this.order[0]).init(option), 0)();
       option.response.end(JSON.stringify(result));
     } catch (error) {
       const exception = error;
@@ -31,12 +31,12 @@ export class HandlerPool {
     }
   }
 
-  static compose(current: BaseHandler, i: number): () => any {
-    if (i + 1 < HandlerPool.order.length) {
-      const next = HandlerPool.take(HandlerPool.order[i]).inherit(current);
-      return async () => current.handle(HandlerPool.compose(next, i + 1)).finally(() => HandlerPool.give(current));
+  compose(current: BaseHandler, i: number): () => any {
+    if (i + 1 < this.order.length) {
+      const next = this.take(this.order[i]).inherit(current);
+      return async () => current.handle(this.compose(next, i + 1)).finally(() => this.give(current));
     } else {
-      return async () => current.handle(current.run.bind(current)).finally(() => HandlerPool.give(current));
+      return async () => current.handle(current.run.bind(current)).finally(() => this.give(current));
     }
   }
 
