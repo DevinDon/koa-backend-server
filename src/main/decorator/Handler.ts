f37b3ddcdf1004a5360b0365ece0a322310dd420
IInfinity <DevinDon@Foxmail.com>
2019-06-08 05:46:20 +0800

    feat(decorator): decorator Handler

--- src/main/decorator/Handler.ts
+++ src/main/decorator/Handler.ts
@@ -1,7 +1,18 @@
-export function Handler(): ClassDecorator {
-  return target => {
-    // const pool: HandlerPool = Injector.generate(HandlerPool);
-    // const handlers: BaseHandler[] = Reflect.getMetadata(MetadataKey.Handler, HandlerPool) || [];
-    // Reflect.defineMetadata(MetadataKey.Handler, handlers, HandlerPool);
+import { MetadataKey } from '../@types';
+import { BaseHandler } from '../handler';
+
+export type HandlerType = Function & typeof BaseHandler;
+
+export function Handler(handler?: typeof BaseHandler): ClassDecorator | MethodDecorator | any {
+  return (target: Function | Object, name?: string | symbol, descriptor?: PropertyDecorator) => {
+    if (target instanceof Function) {
+      const array: HandlerType[] = Reflect.getMetadata(MetadataKey.Handler, target) || [];
+      array.push(handler!);
+      Reflect.defineMetadata(MetadataKey.Handler, array, target);
+    } else {
+      const array: HandlerType[] = Reflect.getMetadata(MetadataKey.Handler, target, name!) || [];
+      array.push(handler!);
+      Reflect.defineMetadata(MetadataKey.Handler, array, target, name!);
+    }
   };
 }
