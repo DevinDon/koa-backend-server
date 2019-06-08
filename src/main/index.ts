c51e3cc11303126e874b00f1a031fa6b88263dc7
IInfinity <DevinDon@Foxmail.com>
2019-06-09 01:28:57 +0800

    perf(index): remove useless code

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,3 @@
-import { Logger } from '@iinfinity/logger';
-import 'reflect-metadata';
-
-export const logger = new Logger('Rester');
 export * from './@types';
 export * from './decorator';
 export * from './handler';
