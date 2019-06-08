a31db9aa8df3da8dac302c3952157ae78fd6c2cf
IInfinity <DevinDon@Foxmail.com>
2019-06-09 01:42:43 +0800

    perf(handler/CoreHandler): perf type declaration

--- src/main/handler/CoreHandler.ts
+++ src/main/handler/CoreHandler.ts
@@ -11,28 +11,28 @@ export class CoreHandler extends BaseHandler {
 
   /** Param injectors, function. */
   private paramInjectors: { [index in ParamInjectionType]: (name: string, route: Route) => any } = {
-    PARAM$HTTP$REQUEST: () => this.request!,
-    PARAM$HTTP$RESPONSE: () => this.response!,
+    PARAM$HTTP$REQUEST: () => this.request,
+    PARAM$HTTP$RESPONSE: () => this.response,
     PARAM$PATH$QUERY: (name: string, route: Route) => {
-      const queryObject = Router.format({ method: this.request!.method as Method, path: this.request!.url! }).queryObject;
+      const queryObject = Router.format({ method: this.request.method as Method, path: this.request.url! }).queryObject;
       return queryObject && queryObject[name];
     },
-    PARAM$PATH$VARIABLE: (name: string, route: Route) => Router.format({ method: this.request!.method as Method, path: this.request!.url! }).pathArray![route.mapping.pathArray!.indexOf(`{{${name}}}`)],
+    PARAM$PATH$VARIABLE: (name: string, route: Route) => Router.format({ method: this.request.method as Method, path: this.request.url! }).pathArray![route.mapping.pathArray!.indexOf(`{{${name}}}`)],
     PARAM$REQUEST$BODY: async (): Promise<any> => new Promise<any>((resolve, reject) => {
       let data: Buffer = Buffer.allocUnsafe(0);
-      this.request!.on('data', (chunk: Buffer) => data = Buffer.concat([data, chunk]));
+      this.request.on('data', (chunk: Buffer) => data = Buffer.concat([data, chunk]));
       // TODO: JSON schema & validate
-      this.request!.on('end', () => {
+      this.request.on('end', () => {
         let result;
-        switch (this.request!.headers['content-type']) {
+        switch (this.request.headers['content-type']) {
           case 'application/json': result = JSON.parse(data.toString()); break;
           default: result = data.toString(); break;
         }
         resolve(result);
       });
-      this.request!.on('error', error => reject(error));
+      this.request.on('error', error => reject(error));
     }),
-    PARAM$REQUEST$HEADER: (value: string) => this.request!.headers[value.toLowerCase()]
+    PARAM$REQUEST$HEADER: (value: string) => this.request.headers[value.toLowerCase()]
   };
 
   /**
@@ -44,14 +44,14 @@ export class CoreHandler extends BaseHandler {
   async handle<T>(next: () => Promise<T>): Promise<T> {
     // TODO: refactor by content-type handler
     // content-type default to application/json
-    this.response!.setHeader('content-type', 'application/json');
+    this.response.setHeader('content-type', 'application/json');
     // if route exist
     if (this.route) {
       /** Parameter injection array. */
       const parameterInjections: ParamInjection[] | undefined = Reflect.getMetadata(MetadataKey.Parameter, this.route.target.prototype, this.route.name);
       /** Arguments, or undefined. */
       this.args = parameterInjections
-        ? parameterInjections.map(v => this.paramInjectors[v.type](v.value, this.route!))
+        ? parameterInjections.map(v => this.paramInjectors[v.type](v.value, this.route))
         : [];
       try {
         // await promise args, such as `body`
@@ -66,7 +66,7 @@ export class CoreHandler extends BaseHandler {
       }
       return next();
     }
-    throw new HTTP404Exception(`Can't ${this.request!.method!.toUpperCase()} ${this.request!.url!}`, { request: this.request!, response: this.response! });
+    throw new HTTP404Exception(`Can't ${this.request.method} ${this.request.url!}`, { request: this.request, response: this.response });
   }
 
 }
