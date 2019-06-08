1b2e279e0a031cb56f112c9a144760ae41c0a2fa
IInfinity <I.INF@Outlook.com>
2019-06-09 01:38:40 +0800

    perf(decorator/Handler): perf type declaration

--- src/main/decorator/Handler.ts
+++ src/main/decorator/Handler.ts
@@ -3,16 +3,16 @@ import { BaseHandler } from '../handler';
 
 export type HandlerType = Function & typeof BaseHandler;
 
-export function Handler(handler?: typeof BaseHandler): ClassDecorator | MethodDecorator | any {
-  return (target: Function | Object, name?: string | symbol, descriptor?: PropertyDecorator) => {
+export function Handler(handler: typeof BaseHandler): ClassDecorator | MethodDecorator | any {
+  return (target: Function | Object, name: string | symbol, descriptor: PropertyDecorator) => {
     if (target instanceof Function) {
       const array: HandlerType[] = Reflect.getMetadata(MetadataKey.Handler, target) || [];
-      array.push(handler!);
+      array.push(handler);
       Reflect.defineMetadata(MetadataKey.Handler, array, target);
     } else {
-      const array: HandlerType[] = Reflect.getMetadata(MetadataKey.Handler, target, name!) || [];
-      array.push(handler!);
-      Reflect.defineMetadata(MetadataKey.Handler, array, target, name!);
+      const array: HandlerType[] = Reflect.getMetadata(MetadataKey.Handler, target, name) || [];
+      array.push(handler);
+      Reflect.defineMetadata(MetadataKey.Handler, array, target, name);
     }
   };
 }
