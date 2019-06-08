a61d46b5be2b28875adfe45677f79ee7acdad256
IInfinity <I.INF@Outlook.com>
2019-06-09 01:34:43 +0800

    perf(decorator): remove Guard & Model

--- src/main/decorator/Model.ts
+++
@@ -1,6 +0,0 @@
-// TODO: JSON schema like fastify
-export interface BaseModel<T> {
-  status: boolean;
-  message: string;
-  content: T;
-}
--- src/main/decorator/index.ts
+++ src/main/decorator/index.ts
@@ -1,6 +1,4 @@
 export * from './Controller';
-export * from './Guard';
 export * from './Handler';
 export * from './Injector';
-export * from './Model';
 export * from './Service';
