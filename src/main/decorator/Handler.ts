04a3c00f11b440c5a9f4c586c8c2b4f449e19578
IInfinity <I.INF@Outlook.com>
2019-06-09 16:46:32 +0800

    style(decorator/Handler): add comments

--- src/main/decorator/Handler.ts
+++ src/main/decorator/Handler.ts
@@ -1,15 +1,26 @@
 import { MetadataKey } from '../@types';
 import { BaseHandler } from '../handler';
 
+/** Handler class type. */
 export type HandlerType = Function & typeof BaseHandler;
 
+/**
+ * Class / Method decorator.
+ *
+ * Specify the handler to use for this controller / method.
+ *
+ * If there are handlers on both controller & method, handlers on method will
+ * run first, and then on controller.
+ *
+ * @param {HandlerType} handler Handler class type.
+ */
 export function Handler(handler: typeof BaseHandler): ClassDecorator | MethodDecorator | any {
   return (target: Function | Object, name: string | symbol, descriptor: PropertyDecorator) => {
-    if (target instanceof Function) {
+    if (target instanceof Function) { // if on class
       const array: HandlerType[] = Reflect.getMetadata(MetadataKey.Handler, target) || [];
       array.push(handler);
       Reflect.defineMetadata(MetadataKey.Handler, array, target);
-    } else {
+    } else { // if on method
       const array: HandlerType[] = Reflect.getMetadata(MetadataKey.Handler, target, name) || [];
       array.push(handler);
       Reflect.defineMetadata(MetadataKey.Handler, array, target, name);
