e39548cdc19a632f44650048413b813a56f4c43e
IInfinity <DevinDon@Foxmail.com>
2019-06-09 16:49:49 +0800

    style(handler/HandlerPool): add comments

--- src/main/handler/HandlerPool.ts
+++ src/main/handler/HandlerPool.ts
@@ -1,27 +1,50 @@
 import { inspect } from 'util';
 import { HandlerType } from '../decorator';
 import { BaseHandler, HandlerOption } from './BaseHandler';
-import { CoreHandler } from './CoreHandler';
+import { ParameterHandler } from './ParamterHandler';
 
+/**
+ * Handler pool.
+ */
 export class HandlerPool {
 
+  /** Maximum number of handlers per category, default to 10000. */
   private max = 100 * 100;
+  /** Pool. */
   private pools: Map<string, BaseHandler[]> = new Map();
 
-  handlerTypes: HandlerType[] = [CoreHandler];
+  /** Handler types, default to `[ExceptionHandler, SchemaHandler, ParameterHandler]` */
+  handlerTypes: HandlerType[] = [ParameterHandler];
 
+  /**
+   * Take one hander instance with special type.
+   *
+   * @param {HandlerType} type Handler type.
+   * @returns {T} A handler instance with special type.
+   */
   take<T = BaseHandler>(type: HandlerType): T {
     const pool = this.pools.get(type.name)! || this.pools.set(type.name, []).get(type.name)!;
     return pool.pop() || new type() as any;
   }
 
+  /**
+   * Give back this handler to pool.
+   *
+   * @param {BaseHandler} handler Handler instance.
+   * @returns {number} Number of handlers in pool.
+   */
   give(handler: BaseHandler): number {
     const pool = this.pools.get(handler.constructor.name)! || this.pools.set(handler.constructor.name, []).get(handler.constructor.name)!;
     return pool.length < this.max ? pool.push(handler.init()) : pool.length;
   }
 
+  /**
+   * Process request.
+   *
+   * @param {HandlerOption} option Handler option.
+   */
   async process(option: HandlerOption): Promise<void> {
-    // TODO: refactor
+    // TODO: refactor by ExceptionHandler
     try {
       const handlerTypes = this.handlerTypes.concat(option.route.handlerTypes || []);
       const result = await this.compose(this.take(handlerTypes[0]).init(option), 0, handlerTypes)();
@@ -33,6 +56,14 @@ export class HandlerPool {
     }
   }
 
+  /**
+   * Compose all handler.
+   *
+   * @param {THandler extends BaseHandler} current Current handler instance.
+   * @param {number} i Index of handler types in this request.
+   * @param {HandlerType[]} handlerTypes Handler types in this request.
+   * @returns {() => any} Composed function.
+   */
   compose(current: BaseHandler, i: number, handlerTypes: HandlerType[]): () => any {
     if (i + 1 < handlerTypes.length) {
       // that is very very very, complex
