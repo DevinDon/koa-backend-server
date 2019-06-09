2140daa7de092c72d0ef20fea31c8a3da01c0e61
IInfinity <I.INF@Outlook.com>
2019-06-09 16:49:45 +0800

    refactor(handler/ParameterHandler): rename CoreHandler to ParameterHandler
    style: add comments

--- src/main/handler/ParamterHandler.ts
+++ src/main/handler/ParamterHandler.ts
@@ -1,24 +1,54 @@
+import { IncomingMessage, ServerResponse } from 'http';
 import { MetadataKey, Method } from '../@types';
-import { ParamInjection, ParamInjectionType } from '../decorator/Controller';
+import { ParamInjection, ParamInjectionType } from '../decorator';
 import { HTTP400Exception, HTTP404Exception } from '../Exception';
 import { Route, Router } from '../Router';
 import { BaseHandler } from './BaseHandler';
 
 /**
- * Core handler.
+ * Parameter handler.
+ *
+ * Inject parameter to controller method.
+ *
+ * @extends BaseHandler
  */
-export class CoreHandler extends BaseHandler {
+export class ParameterHandler extends BaseHandler {
 
-  /** Param injectors, function. */
-  private paramInjectors: { [index in ParamInjectionType]: (name: string, route: Route) => any } = {
-    PARAM$HTTP$REQUEST: () => this.request,
-    PARAM$HTTP$RESPONSE: () => this.response,
-    PARAM$PATH$QUERY: (name: string, route: Route) => {
+  /** Parameter injectors, function. */
+  private parameterInjectors: { [index in ParamInjectionType]: (name: string, route: Route) => any } = {
+    /**
+     * Inject HTTP request instance.
+     *
+     * @returns {IncomingMessage} Request instance.
+     */
+    PARAM$HTTP$REQUEST: (): IncomingMessage => this.request,
+    /**
+     * Inject HTTP response instance.
+     *
+     * @returns {ServerResponse} Response instance.
+     */
+    PARAM$HTTP$RESPONSE: (): ServerResponse => this.response,
+    /**
+     * Inject path query object.
+     *
+     * @returns {string | undefined} Query value of special key, maybe undefined.
+     */
+    PARAM$PATH$QUERY: (name: string): string | undefined => {
       const queryObject = Router.format({ method: this.request.method as Method, path: this.request.url! }).queryObject;
       return queryObject && queryObject[name];
     },
-    PARAM$PATH$VARIABLE: (name: string, route: Route) => Router.format({ method: this.request.method as Method, path: this.request.url! }).pathArray![route.mapping.pathArray!.indexOf(`{{${name}}}`)],
-    PARAM$REQUEST$BODY: async (): Promise<any> => new Promise<any>((resolve, reject) => {
+    /**
+     * Inject path variable.
+     *
+     * @returns {string} Path variable.
+     */
+    PARAM$PATH$VARIABLE: (name: string, route: Route): string => Router.format({ method: this.request.method as Method, path: this.request.url! }).pathArray![route.mapping.pathArray!.indexOf(`{{${name}}}`)],
+    /**
+     * Inject request body object, should await it to get result.
+     *
+     * @returns {Promise<any>} Request body promise object.
+     */
+    PARAM$REQUEST$BODY: (): Promise<any> => new Promise<any>((resolve, reject) => {
       let data: Buffer = Buffer.allocUnsafe(0);
       this.request.on('data', (chunk: Buffer) => data = Buffer.concat([data, chunk]));
       // TODO: JSON schema & validate
@@ -32,14 +62,20 @@ export class CoreHandler extends BaseHandler {
       });
       this.request.on('error', error => reject(error));
     }),
-    PARAM$REQUEST$HEADER: (value: string) => this.request.headers[value.toLowerCase()]
+    /**
+     * Inject special request header.
+     *
+     * @returns {string | string[] | undefined} Header value of special key, or undefined.
+     */
+    PARAM$REQUEST$HEADER: (value: string): string | string[] | undefined => this.request.headers[value.toLowerCase()]
   };
 
   /**
-   * Core handle method.
+   * Parameter handle method.
    *
    * @param {Next<T>} next Next function, to go to next handler.
    * @returns {Promise<T>} Result for this handler.
+   * @throws {HTTP404Exception} Not found exception.
    */
   async handle<T>(next: () => Promise<T>): Promise<T> {
     // TODO: refactor by content-type handler
@@ -51,7 +87,7 @@ export class CoreHandler extends BaseHandler {
       const parameterInjections: ParamInjection[] | undefined = Reflect.getMetadata(MetadataKey.Parameter, this.route.target.prototype, this.route.name);
       /** Arguments, or undefined. */
       this.args = parameterInjections
-        ? parameterInjections.map(v => this.paramInjectors[v.type](v.value, this.route))
+        ? parameterInjections.map(v => this.parameterInjectors[v.type](v.value, this.route))
         : [];
       try {
         // await promise args, such as `body`
@@ -66,6 +102,7 @@ export class CoreHandler extends BaseHandler {
       }
       return next();
     }
+    // throw 404 not found exception
     throw new HTTP404Exception(`Can't ${this.request.method} ${this.request.url!}`, { request: this.request, response: this.response });
   }
 
