c7e02de1bc8a05cc31795d6bc97c7f24b3bcc4b8
IInfinity <I.INF@Outlook.com>
2019-06-10 16:51:59 +0800

    perf(exception): add generic to Exception.content
    feat: use field content to save response content

--- src/main/Exception.ts
+++ src/main/Exception.ts
@@ -18,31 +18,31 @@ interface ExceptionContent {
  * @property {string | undefined} message Message to show.
  * @property {T} content Content.
  */
-export class HTTPException<T = ExceptionContent> {
+export class HTTPException<T = any> {
   constructor(public code: number, public message?: string, public content?: T) { }
 }
 
 /** HTTP 400 Exception: Bad Request. */
-export class HTTP400Exception extends HTTPException {
-  constructor(message: string = 'Bad Request', content?: ExceptionContent) { super(400, message, content); }
+export class HTTP400Exception<T> extends HTTPException<T> {
+  constructor(message: string = 'Bad Request', content?: T) { super(400, message, content); }
 }
 
 /** HTTP 401 Exception: Unauthorized. */
-export class HTTP401Exception extends HTTPException {
-  constructor(message: string = 'Unauthorized', content?: ExceptionContent) { super(401, message, content); }
+export class HTTP401Exception<T> extends HTTPException<T> {
+  constructor(message: string = 'Unauthorized', content?: T) { super(401, message, content); }
 }
 
 /** HTTP 403 Exception: Forbidden. */
-export class HTTP403Exception extends HTTPException {
-  constructor(message: string = 'Forbidden', content?: ExceptionContent) { super(403, message, content); }
+export class HTTP403Exception<T> extends HTTPException<T> {
+  constructor(message: string = 'Forbidden', content?: T) { super(403, message, content); }
 }
 
 /** HTTP 404 Exception: Not Found. */
-export class HTTP404Exception extends HTTPException {
-  constructor(message: string = 'Not Found', content?: ExceptionContent) { super(404, message, content); }
+export class HTTP404Exception<T> extends HTTPException<T> {
+  constructor(message: string = 'Not Found', content?: T) { super(404, message, content); }
 }
 
 /** HTTP 500 Exception: Internal Server Error. */
-export class HTTP500Exception extends HTTPException {
-  constructor(message: string = 'Internal Server Error', content?: ExceptionContent) { super(500, message, content); }
+export class HTTP500Exception<T> extends HTTPException<T> {
+  constructor(message: string = 'Internal Server Error', content?: T) { super(500, message, content); }
 }
