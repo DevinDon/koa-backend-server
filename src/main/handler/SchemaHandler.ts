dd3f57ae17151a7a2e04d520f2a3585e4e4a0663
IInfinity <DevinDon@Foxmail.com>
2019-06-10 17:00:34 +0800

    feat(handler/SchemaHandler): add schema handler draft

---
+++ src/main/handler/SchemaHandler.ts
@@ -0,0 +1,38 @@
+import { BaseHandler } from './BaseHandler';
+
+/**
+ * Schema handler, draft.
+ *
+ * @extends BaseHandler
+ */
+export class SchemaHandler extends BaseHandler {
+
+  async handle(next: () => Promise<any>): Promise<any> {
+    return next()
+      .then(v => {
+        if (typeof v === 'object') {
+          if (v instanceof Buffer) {
+            // content-type should be application/json when v is an object & not instanceof Buffer
+            this.response.setHeader('content-type', 'application/octet-stream');
+            // TODO: Buffer is binary, base64 or something else
+            return v;
+          } else {
+            // content-type should be application/json when v is an object & not instanceof Buffer
+            this.response.setHeader('content-type', 'application/json');
+            // TODO: JSON schema
+            return JSON.stringify(v);
+          }
+        } else if (typeof v === 'string') {
+          // content-type should be text/plain when v is an string
+          this.response.setHeader('content-type', 'text/plain');
+          return v;
+        } else {
+          // content-type should be text/plain when v is other type
+          // like boolean, number, and so on
+          this.response.setHeader('content-type', 'text/plain');
+          return String(v);
+        }
+      });
+  }
+
+}
