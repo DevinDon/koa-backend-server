fee6241336c14f7d3fa94fd2b389018c2f462d50
IInfinity <DevinDon@Foxmail.com>
2019-06-10 17:02:36 +0800

    feat(handler/ExceptionHandler): add exception handler

---
+++ src/main/handler/ExceptionHandler.ts
@@ -0,0 +1,21 @@
+import { HTTP500Exception, HTTPException } from '../Exception';
+import { BaseHandler } from './BaseHandler';
+
+export class ExceptionHandler extends BaseHandler {
+
+  async handle(next: () => Promise<any>): Promise<any> {
+    return next()
+      .catch((exception: HTTPException) => {
+        if (!(exception instanceof HTTPException)) { // default to 500
+          exception = new HTTP500Exception('Internal Server Error');
+        }
+        if (!exception.content) { // response content default to {}
+          exception.content = '{}';
+        }
+        this.response.statusCode = exception.code;
+        this.response.statusMessage = exception.message!;
+        return exception.content;
+      });
+  }
+
+}
