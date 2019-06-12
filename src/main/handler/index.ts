3dee2097b033641f8cf6681f8043e4d42ca8aa25
IInfinity <I.INF@Outlook.com>
2019-06-12 18:28:20 +0800

    perf(index): perf export

--- src/main/handler/index.ts
+++ src/main/handler/index.ts
@@ -1,3 +1,6 @@
 export * from './BaseHandler';
+export * from './ExceptionHandler';
 export * from './HandlerPool';
 export * from './ParamterHandler';
+export * from './RouterHandler';
+export * from './SchemaHandler';
