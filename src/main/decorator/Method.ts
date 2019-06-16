3586721887b0d1187e8aac749455d2f12b620f0d
IInfinity <DevinDon@Foxmail.com>
2019-06-16 16:41:12 +0800

    perf(@types): perf method declaration
    Use enum instead of type declaration.

--- src/main/decorator/Method.ts
+++ src/main/decorator/Method.ts
@@ -12,50 +12,64 @@ function baseMethod(method: Method) {
 }
 
 /**
+ * **CONNECT** Method decorator.
+ *
+ * @param {string} path Path to mapping.
+ */
+export const CONNECT = baseMethod(Method.CONNECT);
+
+/**
  * **DELETE** Method decorator.
  *
  * @param {string} path Path to mapping.
  */
-export const DELETE = baseMethod('DELETE');
+export const DELETE = baseMethod(Method.DELETE);
 
 /**
  * **GET** Method decorator.
  *
  * @param {string} path Path to mapping.
  */
-export const GET = baseMethod('GET');
+export const GET = baseMethod(Method.GET);
 
 /**
  * **HEAD** Method decorator.
  *
  * @param {string} path Path to mapping.
  */
-export const HEAD = baseMethod('HEAD');
+export const HEAD = baseMethod(Method.HEAD);
 
 /**
  * **OPTIONS** Method decorator.
  *
  * @param {string} path Path to mapping.
  */
-export const OPTIONS = baseMethod('OPTIONS');
+export const OPTIONS = baseMethod(Method.OPTIONS);
 
 /**
  * **PATCH** Method decorator.
  *
  * @param {string} path Path to mapping.
  */
-export const PATCH = baseMethod('PATCH');
+export const PATCH = baseMethod(Method.PATCH);
 
 /**
  * **DELETE** Method decorator.
  *
  * @param {string} path Path to mapping.
  */
-export const POST = baseMethod('POST');
+export const POST = baseMethod(Method.POST);
 
 /**
  * **PUT** Method decorator.
  *
  * @param {string} path Path to mapping.
  */
-export const PUT = baseMethod('PUT');
+export const PUT = baseMethod(Method.PUT);
+
+/**
+ * **TRACE** Method decorator.
+ *
+ * @param {string} path Path to mapping.
+ */
+export const TRACE = baseMethod(Method.TRACE);
