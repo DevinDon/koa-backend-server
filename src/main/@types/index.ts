bc990e0801f845a9bed5e4ff7090a85a988d5a77
IInfinity <I.INF@Outlook.com>
2019-06-16 16:41:12 +0800

    perf(@types): perf method declaration
    Use enum instead of type declaration.

--- src/main/@types/index.ts
+++ src/main/@types/index.ts
@@ -1,7 +1,17 @@
 import { HandlerType } from '../decorator';
 
 /** Allowed HTTP methods. */
-export type Method = 'DELETE' | 'GET' | 'HEAD' | 'OPTIONS' | 'PATCH' | 'POST' | 'PUT';
+export enum Method {
+  CONNECT = 'CONNECT',
+  DELETE = 'DELETE',
+  GET = 'GET',
+  HEAD = 'HEAD',
+  OPTIONS = 'OPTIONS',
+  PATCH = 'PATCH',
+  POST = 'POST',
+  PUT = 'PUT',
+  TRACE = 'TRACE',
+}
 
 /**
  * Mapping, with method & path.
