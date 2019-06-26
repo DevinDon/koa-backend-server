39a362d14a0021b40572cec9359e91ff8bc1d002
IInfinity <I.INF@Outlook.com>
2019-06-26 15:16:18 +0800

    refactor(handler/pool): change file name to pool.ts

--- src/main/handler/index.ts
+++ src/main/handler/index.ts
@@ -1,6 +1,6 @@
 export * from './base.handler';
 export * from './exception.handler';
-export * from './handler-pool';
 export * from './paramter.handler';
+export * from './pool';
 export * from './router.handler';
 export * from './schema.handler';
