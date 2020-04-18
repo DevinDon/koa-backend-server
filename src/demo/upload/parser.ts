fa0d465885137563e91763f8dba375c75c71784c
IInfinity <I.INF@Outlook.com>
2020-04-18 15:38:23 +0000

    feat(demo/upload): parser is work

--- src/demo/upload/parser.ts
+++ src/demo/upload/parser.ts
@@ -1,5 +1,5 @@
 export interface Part {
-  raw: Buffer;
+  buffer: Buffer;
   contentDisposition: string;
   contentDispositionName: string;
   contentDispositionFilename?: string;
@@ -10,12 +10,13 @@ export interface Part {
 }
 
 const keymap = {
-  br: 0x0a,
+  cr: 0x0d,
+  lf: 0x0a,
   dash: 0x2d
 };
 
 const regmap = {
-  regular: /([^\s]*): ([^\s]*)/,
+  key: /([^\s]*): ([^\s]*)/,
   contentDisposition: /Content-Disposition: ([^;]*)/,
   contentDispositionName: /[^e]name="(.+?)"/,
   contentDispositionFilename: /filename="(.+?)"/,
@@ -36,86 +37,119 @@ export class FormParser {
     this.boundary = contentType.match(/boundary=(.*)/)![1];
   }
 
-  parse(body: string = this.body) {
+  parse(body: string = this.body): Part[] {
+
     const buffer: Buffer = Buffer.from(body);
-    const info = {
-      partStart: 0,
-      dataStart: 0,
-      lineStart: 0,
-      isHeader: true,
-      contentDisposition: '',
-      contentDispositionName: '',
-      contentDispositionFilename: undefined as any,
-      contentType: undefined as any,
-      contentTypeCharset: undefined as any,
-      contentTransferEncoding: undefined as any
-    };
-    for (let i = 0; i < buffer.length; i++) {
-      if (buffer[i] === keymap.br) {
-        const start = i + 3;
-        const end = start + this.boundary.length;
-        if (
-          buffer.length > end
-          && buffer[i + 1] === keymap.dash
-          && buffer[i + 2] === keymap.dash
-          && buffer.toString('utf8', start, end) === this.boundary
-        ) { // is boundary line, end last part
-          info.isHeader = true;
-          if (i !== 0) {
-            this.parts.push({
-              raw: buffer.slice(info.partStart, i - 1),
-              contentDisposition: info.contentDisposition,
-              contentDispositionName: info.contentDispositionName,
-              contentDispositionFilename: info.contentDispositionFilename,
-              contentTransferEncoding: info.contentTransferEncoding,
-              contentType: info.contentType,
-              contentTypeCharset: info.contentTypeCharset,
-              data: buffer.slice(info.dataStart, i - 1)
-            });
-          }
-          i = end;
-          info.partStart = end + 1;
-          info.lineStart = end + 1;
-        } else { // not boundary line, mark as data
-          if (info.lineStart === i && info.isHeader) {
-            info.isHeader = false;
-            info.dataStart = i + 1;
-          }
-          if (i !== 0 && info.isHeader) { // if is multipart header
-            const line = buffer.toString(undefined, info.lineStart, i - 1);
-            info.lineStart = i + 1;
-            const match = line.match(regmap.regular);
-            switch (match && match[1]) {
-              case 'Content-Disposition':
-                const disposition = line.match(regmap.contentDisposition);
-                info.contentDisposition = disposition && disposition[1] || '';
-                const name = line.match(regmap.contentDispositionName);
-                info.contentDispositionName = name && name[1] || '';
-                const filename = line.match(regmap.contentDispositionFilename);
-                info.contentDispositionFilename = filename && filename[1] || undefined;
-                break;
-              case 'Content-Transfer-Encoding':
-                const encoding = line.match(regmap.regular);
-                info.contentTransferEncoding = encoding && encoding[1];
-                break;
-              case 'Content-Type':
-                const type = line.match(regmap.contentType);
-                info.contentType = type && type[1] || undefined;
-                const charset = line.match(regmap.contentTypeCharset);
-                info.contentTypeCharset = charset && charset[1];
-                break;
-              default: // is not header, maybe exception
-                info.dataStart = i + 1;
-                break;
-            }
-          }
-        }
+    const length = buffer.length;
+    let start = 0;
+    let end = 0;
 
-      } else {
-
+    // Get each part.
+    for (let i = 0; i < length; i++) {
+      if (buffer[i] === keymap.cr) { i++; }
+      if (buffer[i] !== keymap.lf) {
+        continue;
       }
+      if (!this.isBoundary(buffer, i + 1)) {
+        continue;
+      }
+      start += 1 + 2 + this.boundary.length + 1;
+      end = i - 1;
+      const part: Part = {} as any;
+      part.buffer = buffer.slice(start, i - 1);
+      for (let j = 0; j < part.buffer.length; j++) {
+        if (part.buffer[j] === keymap.cr && part.buffer[j + 1] === keymap.lf) {
+          part.data = part.buffer.slice(j + 2, end);
+          this.parts.push(part);
+          break;
+        } else {
+          const lineBuffer = this.readLine(part.buffer, j); // fix utf8 code
+          const line = lineBuffer.toString();
+          const key = line.match(regmap.key)![1];
+          switch (key) {
+            case 'Content-Disposition':
+              const disposition = line.match(regmap.contentDisposition);
+              part.contentDisposition = disposition && disposition[1] || '';
+              const filename = line.match(regmap.contentDispositionFilename);
+              part.contentDispositionFilename = filename && filename[1] || undefined;
+              const name = line.match(regmap.contentDispositionName);
+              part.contentDispositionName = name && name[1] || '';
+              break;
+            case 'Content-Transfer-Encoding':
+              const encoding = line.match(regmap.contentTransferEncoding);
+              part.contentTransferEncoding = encoding && encoding[1] || undefined;
+              break;
+            case 'Content-Type':
+              const type = line.match(regmap.contentType);
+              part.contentType = type && type[1] || undefined;
+              const charset = line.match(regmap.contentTypeCharset);
+              part.contentTypeCharset = charset && charset[1] || undefined;
+              break;
+          }
+          j += lineBuffer.length; // fix utf8 code
+        }
+      }
+      start = i + 1;
     }
+
     return this.parts;
+
+  }
+
+  /**
+   * It is boundary line or not.
+   *
+   * First char should not `\n`.
+   *
+   * @param buffer Source buffer.
+   * @param start Where to start.
+   */
+  isBoundary(buffer: Buffer, start: number): boolean {
+
+    return buffer[start] === keymap.dash
+      && buffer.length > start + 2 + this.boundary.length
+      && buffer[start + 1] === keymap.dash
+      && buffer.toString(undefined, start + 2, start + 2 + this.boundary.length) === this.boundary;
+
+  }
+
+  isData(buffer: Buffer, start: number): boolean {
+
+    return buffer[start] === keymap.lf && buffer[start + 1] === keymap.lf;
+
+  }
+
+  /**
+   * Read a line from buffer.
+   *
+   * First char should not `\n`.
+   *
+   * @param buffer Read from buffer.
+   * @param start Where to start.
+   */
+  readLine(buffer: Buffer, start: number): Buffer {
+
+    const length = buffer.length;
+
+    for (let i = start; i < length; i++) {
+      if (buffer[i] === keymap.cr) { return buffer.slice(start, i + 1); }
+      if (buffer[i] === keymap.lf) { return buffer.slice(start, i); }
+    }
+
+    // The end line.
+    return buffer.slice(start, length);
+
   }
 
 }
+
+const buf = Buffer.from(`aaa-aaa=aaa
+bbb
+ccc
+ddd
+eee`);
+const parser = new FormParser('abc;boundary=###', ``);
+
+const s1 = parser.readLine(buf, 0).toString();
+
+console.log(s1);
