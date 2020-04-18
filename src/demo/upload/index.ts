dc6e5731f1a024c0025ccd8e4d389b8ed9758f71
IInfinity <I.INF@Outlook.com>
2020-04-18 10:16:09 +0000

    feat(demo/parser): form data parser version 1

--- src/demo/upload/index.ts
+++ src/demo/upload/index.ts
@@ -1,4 +1,5 @@
 import { PUT, Rester, View, RequestBody, RequestHeader } from '../../main';
+import { FormParser } from './parser';
 
 @View()
 class UploadView {
@@ -9,12 +10,9 @@ class UploadView {
     @RequestBody() body: string
   ) {
     console.log(`Length: ${body.length}`);
-    console.log(`Content Type: ${contentType}`);
-    const boundary = /boundary=(.*)/.exec(contentType)![1];
     // 解析 FormData 请参考 [深入解析 multipart/form-data](https://www.jianshu.com/p/29e38bcc8a1d)
-    const formParser = new FormParser(contentType, body);
-    // TODO: 优化解析方案，实现逐行开头字符匹配减少开销
-    return results.toString();
+    const parts = new FormParser(contentType, body).parse();
+    return parts;
   }
 
 }
--- src/demo/upload/parser.ts
+++ src/demo/upload/parser.ts
@@ -1,9 +1,11 @@
 export interface Part {
   raw: Buffer;
-  name: string;
-  filename: string;
-  contentType: string;
-  contentTransferEncoding: string;
+  contentDisposition: string;
+  contentDispositionName: string;
+  contentDispositionFilename?: string;
+  contentTransferEncoding?: string;
+  contentType?: string;
+  contentTypeCharset?: string;
   data: Buffer;
 }
 
@@ -12,57 +14,108 @@ const keymap = {
   dash: 0x2d
 };
 
+const regmap = {
+  regular: /([^\s]*): ([^\s]*)/,
+  contentDisposition: /Content-Disposition: ([^;]*)/,
+  contentDispositionName: /[^e]name="(.+?)"/,
+  contentDispositionFilename: /filename="(.+?)"/,
+  contentType: /Content-Type: ([^;]*)/,
+  contentTypeCharset: /charset=([^;]*)/,
+  contentTransferEncoding: /Content-Transfer-Encoding: ([^;]*)/
+};
+
 export class FormParser {
 
   private boundary: string;
   private parts: Part[] = [];
 
   constructor(
-    private contentType: string,
+    contentType: string,
     private body: string
   ) {
     this.boundary = contentType.match(/boundary=(.*)/)![1];
   }
 
-  parse(body: string) {
+  parse(body: string = this.body) {
     const buffer: Buffer = Buffer.from(body);
     const info = {
       partStart: 0,
-      partEnd: 0,
-      isData: false,
-      name: undefined,
-      filename: undefined,
-      contentType: undefined,
-      contentTransferEncoding: undefined,
       dataStart: 0,
-      dataEnd: 0
+      lineStart: 0,
+      isHeader: true,
+      contentDisposition: '',
+      contentDispositionName: '',
+      contentDispositionFilename: undefined as any,
+      contentType: undefined as any,
+      contentTypeCharset: undefined as any,
+      contentTransferEncoding: undefined as any
     };
-    let partStart = 0;
     for (let i = 0; i < buffer.length; i++) {
-      switch (buffer[i]) {
-        case keymap.br:
-          const start = i + 3;
-          const end = start + this.boundary.length;
-          if (
-            buffer.length > end
-            && buffer[i + 1] === keymap.dash
-            && buffer[i + 2] === keymap.dash
-            && buffer.toString('utf8', start, end) === this.boundary
-          ) { // is boundary line, end last part
+      if (buffer[i] === keymap.br) {
+        const start = i + 3;
+        const end = start + this.boundary.length;
+        if (
+          buffer.length > end
+          && buffer[i + 1] === keymap.dash
+          && buffer[i + 2] === keymap.dash
+          && buffer.toString('utf8', start, end) === this.boundary
+        ) { // is boundary line, end last part
+          info.isHeader = true;
+          if (i !== 0) {
             this.parts.push({
-              raw: buffer.slice(info.partStart, info.partEnd)
+              raw: buffer.slice(info.partStart, i - 1),
+              contentDisposition: info.contentDisposition,
+              contentDispositionName: info.contentDispositionName,
+              contentDispositionFilename: info.contentDispositionFilename,
+              contentTransferEncoding: info.contentTransferEncoding,
+              contentType: info.contentType,
+              contentTypeCharset: info.contentTypeCharset,
+              data: buffer.slice(info.dataStart, i - 1)
             });
-            partStart = end;
-            i = end;
-          } else { // not boundary line, mark as data
-
           }
-          break;
+          i = end;
+          info.partStart = end + 1;
+          info.lineStart = end + 1;
+        } else { // not boundary line, mark as data
+          if (info.lineStart === i && info.isHeader) {
+            info.isHeader = false;
+            info.dataStart = i + 1;
+          }
+          if (i !== 0 && info.isHeader) { // if is multipart header
+            const line = buffer.toString(undefined, info.lineStart, i - 1);
+            info.lineStart = i + 1;
+            const match = line.match(regmap.regular);
+            switch (match && match[1]) {
+              case 'Content-Disposition':
+                const disposition = line.match(regmap.contentDisposition);
+                info.contentDisposition = disposition && disposition[1] || '';
+                const name = line.match(regmap.contentDispositionName);
+                info.contentDispositionName = name && name[1] || '';
+                const filename = line.match(regmap.contentDispositionFilename);
+                info.contentDispositionFilename = filename && filename[1] || undefined;
+                break;
+              case 'Content-Transfer-Encoding':
+                const encoding = line.match(regmap.regular);
+                info.contentTransferEncoding = encoding && encoding[1];
+                break;
+              case 'Content-Type':
+                const type = line.match(regmap.contentType);
+                info.contentType = type && type[1] || undefined;
+                const charset = line.match(regmap.contentTypeCharset);
+                info.contentTypeCharset = charset && charset[1];
+                break;
+              default: // is not header, maybe exception
+                info.dataStart = i + 1;
+                break;
+            }
+          }
+        }
 
-        default:
-          break;
+      } else {
+
       }
     }
+    return this.parts;
   }
 
 }
