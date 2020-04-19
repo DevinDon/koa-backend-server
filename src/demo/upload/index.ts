d0e339cc92deeecbdc6e7974c3f49ba2303d4387
IInfinity <I.INF@Outlook.com>
2020-04-19 10:23:42 +0000

    feat(demo/upload): add upload demo

--- src/demo/upload/index.ts
+++ src/demo/upload/index.ts
@@ -1,19 +1,30 @@
-import { PUT, RequestBody, Rester, View } from '../../main';
+import { writeFileSync } from 'fs';
+import { CORSHandler, Handler, Part, PUT, RequestBody, Rester, View } from '../../main';
 
 @View()
+@Handler(CORSHandler)
 class UploadView {
 
   @PUT('/')
   index(
-    @RequestBody() body: any
+    @RequestBody() body: Part[]
   ) {
     console.log(`Length: ${body.length}`);
     console.log(body);
-    return body;
+    body.forEach(v => v.contentDispositionFilename && writeFileSync('temp/' + v.contentDispositionFilename + '.txt', v.data));
+    return body.map(v => ({ file: v.contentDispositionFilename, size: v.data.length }));
+  }
+
+  @PUT('/single')
+  single(@RequestBody() body: Buffer) {
+    console.log(`Length: ${body.length}`);
+    console.log(body);
+    writeFileSync('temp/' + Math.random(), body);
+    return body.length;
   }
 
 }
 
 const server = new Rester()
   .configViews.add(UploadView).end()
-  .listen(() => console.log('Server started.'), 8080, '0.0.0.0');
+  .listen(() => { }, 8080, '0.0.0.0');
