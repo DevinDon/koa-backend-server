c4fa60ec0e450a442d4fce1d3b430862eab10441
Devin Don <I.INF@Outlook.com>
2020-05-15 08:15:50 +0800

    feat(demo): update demo

--- src/demo/simple/index.ts
+++ src/demo/simple/index.ts
@@ -1,15 +1,28 @@
-import { GET, Rester, View } from '../../main';
+import { GET, PathVariable, Rester, View, baseParam, parameterInjectors, BaseHandler } from '../../main';
+
+const Test = baseParam('abc');
+
+parameterInjectors.abc = (handler) => {
+  return handler.request.headers;
+}
 
 @View()
 class DemoView {
 
-  @GET('/')
+  @GET()
   index() {
     return { hello: 'world' };
+  }
+
+  @GET('{{name}}')
+  name(@Test() name: string) {
+    const a = name + 1;
+    return name;
   }
 
 }
 
 const server = new Rester()
+  .configAddress.setHost('0.0.0.0').end()
   .configViews.add(DemoView).end()
   .listen();
