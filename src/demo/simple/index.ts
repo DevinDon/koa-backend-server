7ed17179ca0853f7e6a938836f0474ba251cd6dd
Devin Don <I.INF@Outlook.com>
2020-08-30 22:02:29 +0800

    feat(demo): add demo

--- src/demo/simple/index.ts
+++ src/demo/simple/index.ts
@@ -1,32 +1,12 @@
-import { logger } from '@iinfinity/logger';
-import { BaseHandler, ExceptionHandler, GET, Handler, HandlerZone, ParameterHandler, PathVariable, Rester, RouterHandler, SchemaHandler, View } from '../../main';
+import { ExceptionHandler, GET, Handler, HandlerZone, ParameterHandler, PathVariable, Rester, RouterHandler, SchemaHandler, View } from '../../main';
 
-class ZoneHandler extends BaseHandler {
-
-  async handle(next: () => Promise<any>): Promise<any> {
-    this.zone = { hello: 'world' };
-    logger.debug('handler works');
-    return next();
-  }
-
-}
 
 @View()
 class DemoView {
 
   @GET()
   index() {
-    return { hello: 'world' };
-  }
-
-  @Handler(ZoneHandler)
-  @GET('{{name}}')
-  name(
-    @PathVariable('name') name: string,
-    @HandlerZone() zone: { hello: 'world' },
-    @HandlerZone('what') what: string
-  ) {
-    return { ...zone, name, what };
+    return 'ok';
   }
 
 }
@@ -34,5 +14,4 @@ class DemoView {
 const server = new Rester()
   .configAddress.setHost('0.0.0.0').end()
   .configViews.add(DemoView).end()
-  .configHandlers.set(ExceptionHandler, SchemaHandler, RouterHandler, ZoneHandler, ParameterHandler).end()
   .listen();
