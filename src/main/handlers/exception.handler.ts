28f4eb3e5214b1be5b9e8435da1926447ad28ee8
Devin Don <i.inf@outlook.com>
2021-03-01 09:51:53 +0800

    fix(handlers/exception): fix exception handler with `trace` config

--- src/main/handlers/exception.handler.ts
+++ src/main/handlers/exception.handler.ts
@@ -9,13 +9,11 @@ export interface ExceptionConfig {
 
 export class ExceptionHandler extends BaseHandler {
 
-  static config(rester: Rester, config?: ExceptionConfig): HandlerType {
-    rester.zone.exception = config || { response: JSON.stringify({ status: false }) };
-    return ExceptionHandler;
-  }
-
-  static init(rester: Rester, config?: any): HandlerType {
-    ExceptionHandler.config(rester, config);
+  static init(rester: Rester): HandlerType {
+    ExceptionHandler.config({
+      trace: rester.config.logger.trace,
+      response: JSON.stringify({ status: false }),
+    });
     return ExceptionHandler;
   }
 
@@ -26,37 +24,40 @@ export class ExceptionHandler extends BaseHandler {
     return next()
       .catch((exception: HTTPException | Error) => {
 
+        const trace = ExceptionHandler.configuration.trace;
+
         let returns;
-        const debug = this.rester.zone.config?.debug;
 
         if (exception instanceof HTTPException) {
           // if it is HTTP Exception, set code & return the content
           this.logger
             .error(`HTTP ${exception.code} Exception from '${this.request.method} ${this.request.url}': ${exception.message}\t${exception.content ? JSON.stringify(exception.content) : ''}`);
-          // if debug is true, output stack info
-          if (debug) {
+          // if trace is true, output stack info
+          if (trace) {
             this.logger.error(`Detail: ${exception.stack}`);
           }
           this.response.statusCode = exception.code;
           this.response.statusMessage = exception.message;
           returns = exception.content;
         } else {
-          // else, just throw 500 with `zone.exception.response` or `{}`
+          // else, just throw 500 with `ExceptionHandler.configuration.response`
           this.logger
             .error(`HTTP 500 Internal Exception from '${this.request.method} ${this.request.url}': ${exception.name} ${exception.message}`);
-          // if debug is true, output stack info
-          if (debug) {
+          // if trace is true, output stack info
+          if (trace) {
             this.logger.error(`Detail: ${exception.stack}`);
           }
           this.response.statusCode = 500;
           this.response.statusMessage = exception.message;
-          returns = this.rester.zone.exception?.response || {};
+          returns = ExceptionHandler.configuration.response;
         }
 
+        // default to `application/json` header
         if (!this.response.getHeader('Content-Type')) {
           this.response.setHeader('Content-Type', 'application/json');
         }
 
+        // stringify without schema
         return typeof returns === 'string'
           ? returns
           : JSON.stringify(returns);
--- src/main/handlers/logger.handler.ts
+++ src/main/handlers/logger.handler.ts
@@ -4,24 +4,11 @@ import { BaseHandler } from '../handlers/base.handler';
 
 export class LoggerHandler extends BaseHandler {
 
-  static trace: boolean = false;
-
   private logger = this.rester.logger;
-
-  /**
-   * Config & init handler.
-   *
-   * @param {Rester} rester Rester instance.
-   * @returns {HandlerType} Handler class.
-   */
-  static init(rester: Rester): HandlerType {
-    LoggerHandler.configuration.trace = rester.config.logger.trace;
-    return this;
-  }
 
   async handle(next: () => Promise<any>): Promise<any> {
     this.logger.debug(`${this.mapping.method} ${this.mapping.path} ${this.mapping.query || ''}`);
-    return next().catch(exception => LoggerHandler.configuration.trace && this.logger.error(exception));
+    return next();
   }
 
 }
