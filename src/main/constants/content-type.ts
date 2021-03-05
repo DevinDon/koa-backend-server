d7105d2609ac35075990fc43c5616cd685a1664e
Devin Don <i.inf@outlook.com>
2021-03-06 02:28:35 +0800

    feat(constants): add constants

---
+++ src/main/constants/content-type.ts
@@ -0,0 +1,17 @@
+export const CONTENT_TYPE = 'content-type';
+
+export enum ContentType {
+
+  HTML = 'text/html',
+  CSS = 'text/css',
+  JavaScript = 'application/x-javascript',
+  TEXT = 'text/plain',
+  GIF = 'text/gif',
+  JPG = 'text/jpeg',
+  PNG = 'text/png',
+  XML = 'application/xml',
+  JSON = 'application/json',
+  PDF = 'application/pdf',
+  STREAM = 'application/octet-stream'
+
+}
