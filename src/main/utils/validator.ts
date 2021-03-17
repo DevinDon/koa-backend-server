c9c29b702167d3f284f135fbbe581f49a7752ff3
Devin Don <i.inf@outlook.com>
2021-03-17 14:51:06 +0800

    perf(validator): add validator

--- src/main/utils/validator.ts
+++ src/main/utils/validator.ts
@@ -1,14 +1,20 @@
 import { HTTP400Exception } from '../exceptions';
 
-export const requiredParams = <T = any>(params: T): any => {
-  for (const key in params) {
-    if (params[key] === undefined || params[key] === null) {
-      throw new HTTP400Exception(`parameters '${Object.keys(params).join(', ')}' are required`);
+export const requiredParam = <T = any>(param: T): void => {
+  if (param === undefined || param === null) {
+    throw new HTTP400Exception('parameter are required');
+  }
+};
+
+export const requiredParams = <TArray extends []>(params: TArray): void => {
+  for (const param of params) {
+    if (params === undefined || params === null) {
+      throw new HTTP400Exception('parameters are required');
     }
   }
 };
 
-export const requiredParamsInFields = <T = any>(params: T, fields: (keyof T)[]): any => {
+export const requiredParamsInFields = <TObject extends {}>(params: TObject, fields: (keyof TObject)[]): void => {
   for (const field of fields) {
     if (params[field] === undefined || params[field] === null) {
       throw new HTTP400Exception(`params '${fields.join(', ')}' are required`);
