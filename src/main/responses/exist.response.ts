8fed48ba3d7d85b48bbd76dc90f6bf42d4514e99
Devin Don <i.inf@outlook.com>
2021-03-25 18:50:14 +0800

    feat(response): add exist fields

--- src/main/responses/exist.response.ts
+++ src/main/responses/exist.response.ts
@@ -1,15 +1,21 @@
 import { HTTP404Exception } from '../exceptions';
 import { ResterResponse, ResponseConfig } from './rester.response';
 
-export type ExistResponseConfig = Partial<ResponseConfig> & {
+export type ExistResponseConfig<T> = Partial<ResponseConfig<T>> & {
   message?: string;
+  fields?: (keyof T)[];
 };
 
-export class ExistResponse extends ResterResponse {
+export class ExistResponse<T> extends ResterResponse<T> {
 
-  constructor({ data, message = 'Resource not found.', ...rest }: ExistResponseConfig) {
-    if (!data) {
+  constructor({ data, message = 'Resource not found.', fields, ...rest }: ExistResponseConfig<T>) {
+    if (data === undefined || data == null) {
       throw new HTTP404Exception(message);
+    }
+    if (fields) {
+      data = fields
+        .map(field => ({ [field]: data![field] }))
+        .reduce((prev, next) => Object.assign({}, prev, next)) as any;
     }
     super({ data, ...rest });
   }
