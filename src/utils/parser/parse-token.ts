5eac015c0f0bc6d034e539d15ad796f230ae167d
Devin Don <i.inf@outlook.com>
2021-04-14 16:13:29 +0800

    perf(utils/parser): update parse-token

--- src/utils/parser/parse-token.ts
+++ src/utils/parser/parse-token.ts
@@ -1,7 +1,7 @@
 import { IncomingMessage } from 'http';
 import { HTTP401Exception } from '../../exceptions';
 
-export const parseToken = (input?: string) => {
+export const parseToken = (input: string) => {
 
   if (typeof input !== 'string') {
     throw new HTTP401Exception('Invalid token.');
@@ -17,5 +17,5 @@ export const parseToken = (input?: string) => {
 };
 
 export const parseTokenFromRequest = (request: IncomingMessage) => {
-  return parseToken(request.headers['authorization']);
+  return parseToken(request.headers['authorization']!);
 };
