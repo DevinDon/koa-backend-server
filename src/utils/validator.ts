ef88eba602b75cde978fad8cf6b6110e3e2a86b6
Devin Don <i.inf@outlook.com>
2021-05-06 11:04:32 +0800

    perf(utils/validator): update validator

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,55 +1,54 @@
 import { HTTP400Exception } from '../exceptions';
 
-export const requiredParam = <T = any>(param: T): void => {
-  if (param === undefined || param === null) {
-    throw new HTTP400Exception('Parameters are required.');
+export const requiredParamsInFields = <T>(params: T, fields: (keyof T)[]): void => {
+
+  const missingParams: (keyof T)[] = fields
+    .map(field => params[field] === undefined || params[field] === null ? field : undefined as any)
+    .filter(missing => missing !== undefined);
+
+  if (missingParams.length > 0) {
+    throw new HTTP400Exception(`Parameters '${missingParams.join(', ')}' are required.`);
   }
+
 };
 
-export const requiredParams = <T extends Array<any>>(...params: T): void => {
-  for (const param of params) {
-    if (param === undefined || param === null) {
-      throw new HTTP400Exception('Parameters are required.');
-    }
-  }
+export const requiredParams = <T>(params: T): void => {
+
+  const fields = Object.keys(params) as (keyof T)[];
+
+  requiredParamsInFields(params, fields);
+
 };
 
-export const requiredParamsInFields = <T>(params: T, fields?: (keyof T)[]): void => {
-  if (fields === undefined) {
-    fields = Object.keys(params) as (keyof T)[];
-  }
-  for (const field of fields) {
-    if (params[field] === undefined || params[field] === null) {
-      throw new HTTP400Exception(`Parameters '${fields.join(', ')}' are required.`);
-    }
-  }
-};
+export const requiredAtLeastOneParam = <T>(params: T): void => {
 
-export const requiredAtLeastOneParam = <T extends Array<any>>(...params: T) => {
+  const fields = Object.keys(params) as (keyof T)[];
 
-  const isAllUndefined = params
-    .map(param => param === undefined ? 1 : 0 as number)
-    .reduce((prev, curr) => prev + curr) === params.length;
+  const isAllUndefined = fields
+    .map(field => params[field] === undefined || params[field] === null ? 0 : 1 as number)
+    .reduce((prev, next) => prev + next) === 0;
 
   if (isAllUndefined) {
-    throw new HTTP400Exception('There should be at least one parameter that is not undefined.');
+    throw new HTTP400Exception(`At least one of these parameters '${fields.join(', ')}' should be defined.`);
   }
 
 };
 
-export const requiredAtMostOneParam = <T extends Array<any>>(...params: T) => {
+export const requiredAtMostOneParam = <T>(params: T): void => {
 
-  const isMoreDefined = params
-    .map(param => param === undefined ? 0 : 1 as number)
-    .reduce((prev, curr) => prev + curr) > 1;
+  const fields = Object.keys(params) as (keyof T)[];
+
+  const isMoreDefined = fields
+    .map(field => params[field] === undefined || params[field] === null ? 0 : 1 as number)
+    .reduce((prev, next) => prev + next) > 1;
 
   if (isMoreDefined) {
-    throw new HTTP400Exception('There should be at most one parameter that is defined.');
+    throw new HTTP400Exception(`At most one of these parameters '${fields.join(', ')}' should be defined.`);
   }
 
 };
 
-export const requiredInRange = (min: number, value: number, max: number) => {
+export const requiredInRange = ({ min, value, max }: { min: number, value: number, max: number }): void => {
 
   const isNotInRange = value < min || value > max;
 
